78b92ff94f7fd8ab92e02e81b8c0d78bd941339c
Jens Edhammer <tobyndax@hotmail.com>
2014-04-07 12:43:10 +0200

    small array program

---
+++ Kod/array.cc
@@ -0,0 +1,19 @@
+
+#include <fstream>      // std::ofstream
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+int main () {
+    
+    unsigned char* bla = (unsigned char*)"blabla";
+    
+    unsigned char data[10] ={'4','B','C','D','E'};
+    int s = strlen((char*)data);
+    cout << "Antal använda tecken: " << s << endl;
+    
+    
+    cout << "Max storlek på array: " << sizeof(data)<< endl;
+    
+    return 0;
+}
