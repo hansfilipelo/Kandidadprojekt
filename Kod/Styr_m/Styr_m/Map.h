bcbd31227cee4cb469b24dee36560496122488fd
Niklas Ericson <niklasericsons@gmail.com>
2014-04-11 15:07:33 +0200

    added styr_m directory

---
+++ Kod/Styr_m/Styr_m/Map.h
@@ -0,0 +1,42 @@
+#ifndef _Map_h
+#define _Map_h
+
+#ifndef __AVR_ATmega1284P__
+#define DEBUG 1
+#else
+#define DEBUG 0
+#endif
+
+#if DEBUG == 1
+#include <iostream>
+#endif
+
+#include "Abstraction.h"
+#include <string.h>
+#include <stdio.h>
+
+class MapSection;
+class Robot;
+
+class Map
+{
+public:
+    // Construct
+    Map();
+    ~Map();
+	
+	void setSection(int, int, MapSection*);
+	void convertSection(int,int,char);
+	char* getColAsChar(int);
+	
+	MapSection* getPos(int,int);
+    
+#if DEBUG == 1
+    void printMap();
+#endif
+	
+protected:
+	MapSection* mapArea[32][17];
+};
+
+#endif
