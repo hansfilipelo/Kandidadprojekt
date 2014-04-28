c2e134d4d8ca5134697252509012efc677052453
Jens Edhammer <tobyndax@hotmail.com>
2014-04-28 12:32:36 +0200

    changed separator sign to 30 for now.

--- Kod/Kommunikation/serialreader/order.cpp
+++ Kod/Kommunikation/serialreader/order.cpp
@@ -18,53 +18,53 @@ Order::~Order(){
  */
 
 //void Order::setPID(int kp, int kd){
-//    char data[27] = {4,'P',0,(char)kp,(char)kd,'Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z'};
+//    char data[27] = {4,'P',0,(char)kp,(char)kd,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30};
 //    serport->sendArray(data);
 //}
 
 void Order::rotateLeft(int speed){
 
-    char data[27] = {3,'r',0,(char)speed,'Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z'};
+    char data[27] = {3,'r',0,(char)speed,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30};
     serport->sendArray(data);
 }
 
 
 void Order::rotateRight(int speed){
 
-    char data[27] = {3,'r',1,(char)speed,'Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z'};
+    char data[27] = {3,'r',1,(char)speed,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30};
     serport->sendArray(data);
 }
 
 
 void Order::halt(){
     
-    char data[27] = {1,'h','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z'};
+    char data[27] = {1,'h',30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30};
     serport->sendArray(data);
 }
 
 
 void Order::forward(int speed){
 
-    char data[27] = {3,'f','Z',(char)speed,'Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z'};
+    char data[27] = {3,'f',30,(char)speed,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30};
     serport->sendArray(data);
 }
 
 
 void Order::backward(int speed){
 
-    char data[27] = {3,'b','Z',(char)speed,'Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z'};
+    char data[27] = {3,'b',30,(char)speed,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30};
     serport->sendArray(data);
 }
 
 void Order::test(){
     
-    char data[27] = {4,'B','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z'};
+    char data[27] = {4,'B',30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30};
     serport->sendArray(data);
 }
 
 void Order::fetchMap(){
 
-    char data[27] = {1,'F','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z'};
+    char data[27] = {1,'F',30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30};
     serport->sendArray(data);
 }
 
@@ -78,6 +78,8 @@ void Order::setControlParameters(double Kp, double Kd, int Ref){
     int Kd01=floor(10*Kd);
     int Kd001=floor(100*Kd);
 
-    char data[27] = {9,'P',0,(char)Kp1,(char)Kp01,(char)Kp001,(char)Kd1,(char)Kd01,(char)Kd001,(char)Ref,'Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z','Z'};
+    std::cout << kp1 << kp01 << kp001 << std::endl;
+
+    char data[27] = {9,'P',0,(char)Kp1,(char)Kp01,(char)Kp001,(char)Kd1,(char)Kd01,(char)Kd001,(char)Ref,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30};
     serport->sendArray(data);
 }
