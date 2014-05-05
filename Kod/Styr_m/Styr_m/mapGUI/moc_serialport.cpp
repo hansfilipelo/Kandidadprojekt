6feba5e1372c9bdf6aa4a43029c6a1a841c345ce
Jens Edhammer <tobyndax@hotmail.com>
2014-05-05 15:27:15 +0200

    MapGUI small changes. Speed multiplier didn't update. And reference set to 10 cm

---
+++ Kod/Styr_m/Styr_m/mapGUI/moc_serialport.cpp
@@ -0,0 +1,120 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'serialport.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.3.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../Kommunikation/serialreader/serialport.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'serialport.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.3.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_SerialPort_t {
+    QByteArrayData data[7];
+    char stringdata[86];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_SerialPort_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_SerialPort_t qt_meta_stringdata_SerialPort = {
+    {
+QT_MOC_LITERAL(0, 0, 10),
+QT_MOC_LITERAL(1, 11, 15),
+QT_MOC_LITERAL(2, 27, 0),
+QT_MOC_LITERAL(3, 28, 11),
+QT_MOC_LITERAL(4, 40, 28),
+QT_MOC_LITERAL(5, 69, 5),
+QT_MOC_LITERAL(6, 75, 10)
+    },
+    "SerialPort\0handleReadyRead\0\0handleError\0"
+    "QSerialPort::SerialPortError\0error\0"
+    "handleData"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_SerialPort[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       3,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   29,    2, 0x08 /* Private */,
+       3,    1,   30,    2, 0x08 /* Private */,
+       6,    1,   33,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 4,    5,
+    QMetaType::Void, QMetaType::QByteArray,    2,
+
+       0        // eod
+};
+
+void SerialPort::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        SerialPort *_t = static_cast<SerialPort *>(_o);
+        switch (_id) {
+        case 0: _t->handleReadyRead(); break;
+        case 1: _t->handleError((*reinterpret_cast< QSerialPort::SerialPortError(*)>(_a[1]))); break;
+        case 2: _t->handleData((*reinterpret_cast< QByteArray(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject SerialPort::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_SerialPort.data,
+      qt_meta_data_SerialPort,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *SerialPort::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *SerialPort::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_SerialPort.stringdata))
+        return static_cast<void*>(const_cast< SerialPort*>(this));
+    return QObject::qt_metacast(_clname);
+}
+
+int SerialPort::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 3)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 3;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 3)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 3;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
