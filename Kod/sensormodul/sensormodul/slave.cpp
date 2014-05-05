52a9aaa112d7a2fcea4dc5fcdb8d8c86e93e2409
habrman <davha227@student.liu.se>
2014-05-05 13:15:16 +0200

    Merge branch 'master' of https://github.com/hansfilipelo/MapMaster2001
    Conflicts:
    	Kod/sensormodul/sensormodul.atsuo
    	Kod/sensormodul/sensormodul/sensormodul.cpp

--- Kod/sensormodul/sensormodul/slave.cpp
+++ Kod/sensormodul/sensormodul/slave.cpp
@@ -9,56 +9,35 @@
 
 void Slave::SPI_Init()
 {
-	/* Set MISO output, all others input */
-	DDRB = (1<<DDB6);
+	DDRB = (1<<DDB6);	// Set MISO output, all others input
 	/* Enable SPI */
 	SPCR = (1<<SPE)|(1<<SPIE)|(1<<SPR0);
 	SPCR &= ~(1<<MSTR);
 	
-	//PORTB |= (1<<PORTB4); //Enable pull-up resistor
-	
-	//dummy läsning för att rensa registret
-	SPDR=0x00;
+	SPDR=0x00;		//reset SPDR
 	
 	
-	EIMSK |= (1<<INT0);
+	EIMSK |= (1<<INT0);		//enable external interrupt
 	EICRA |= (1<<ISC01)|(1<<ISC00);
 	
-	DDRB |= (1<<DDB2);
+	DDRB |= (1<<DDB2);	//set port B2 to output
 	
-	DDRD &= ~(1<<DDD7);
+	DDRD &= ~(1<<DDD7); //set port D7 to input
 	
-	//sets interupt on pcint16
+	//sets internal interrupt on pcint16
 	PCMSK2 |= (1<<PCINT16);
 	PCICR |= (1<<PCIE2);	
 	DDRC |= (1<<DDC0);
 	
-	position = 0;
+	position = 0;		//reset position
 	return;
 }
 
 void Slave::SPI_Send(){
-	slaveSend = true;
-	position=0;
-	SPDR = outDataArray[0];
+	slaveSend = true;		//slave is sending
+	position=0;				//reset position
+	SPDR = outDataArray[0];	//sent first int in outDataArray
 	PORTB |= (1<<PORTB2);	//interrupt to master
 	PORTB &= ~(1<<PORTB2);	//clear interrupt to master
 	return;
 }
-
-/*
-ISR(SPI_STC_vect){
-	sensormodul.position++;
-	SPDR = sensormodul.outDataArray[sensormodul.position];
-	sensormodul.inDataArray[sensormodul.position-1] = SPDR;
-	
-	if ((sensormodul.position == (sensormodul.inDataArray[0]+1))&(sensormodul.inDataArray[0]!= 0)){
-		PORTC |= (1<<PORTC0);
-	}
-}
-
-ISR(PCINT2_vect){
-	handleInDataArray();
-}
-
-*/
