a04141bc5144bb20e5ccd13648753b9f88041673
Jens Edhammer <tobyndax@hotmail.com>
2014-06-03 19:19:28 +0200

    Fixed so the correct pdfs are included. Makekappa.bash now outputs text to file and then greps all warnings. This however will not work with faults.

--- Skrivuppgifter/Kappa/makekappa.bash
+++ Skrivuppgifter/Kappa/makekappa.bash
@@ -2,16 +2,19 @@
 # cd to script dir
 DIR=$( cd "$( dirname "$0" )" && pwd )
 cd $DIR
+touch latexout.txt
 
 cd ../Kommunikation
-pdflatex kom.tex
+pdflatex kom.tex > $DIR/latexout.txt 2>&1
 cd ../Reglering
-pdflatex regler.tex
+pdflatex regler.tex >> $DIR/latexout.txt 2>&1
 cd ../Sensorer
-pdflatex Sensorer.tex
+pdflatex Sensorer.tex >> $DIR/latexout.txt 2>&1
 cd ../Teknisk_Dokumentation
-pdflatex tekdok.tex
+pdflatex tekdok.tex >> $DIR/latexout.txt 2>&1
 cd ../../Designspec
-pdflatex Designspec.tex 
+pdflatex Designspec.tex >> $DIR/latexout.txt 2>&1
 cd $DIR
-pdflatex kappa.tex
+pdflatex kappa.tex >> latexout.txt 2>&1 
+grep -w "Warning:" latexout.txt
+
