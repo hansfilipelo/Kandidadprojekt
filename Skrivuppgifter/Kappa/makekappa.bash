6b11d512cd5290a06a0962f17cb4d8ae8759c3b4
hansfilipelo <hansfilipelo@gmail.com>
2014-06-05 20:56:48 +0200

    Cleanup in script

--- Skrivuppgifter/Kappa/makekappa.bash
+++ Skrivuppgifter/Kappa/makekappa.bash
@@ -1,22 +1,32 @@
 #!/bin/bash
+
+# Variables
 # cd to script dir
 DIR=$( cd "$( dirname "$0" )" && pwd )
 cd $DIR
-touch latexout.txt
+# OUT
+OUTFILE=latexout.txt
+
+touch $OUTFILE
 
 
 cd ../Kommunikation
-pdflatex -interaction=nonstopmode kom.tex > $DIR/latexout.txt 2>&1
+pdflatex -interaction=nonstopmode kom.tex > $DIR/$OUTFILE 2>&1
 cd ../Reglering
-pdflatex -interaction=nonstopmode regler.tex >> $DIR/latexout.txt 2>&1
+pdflatex -interaction=nonstopmode regler.tex >> $DIR/$OUTFILE 2>&1
 cd ../Sensorer
-pdflatex -interaction=nonstopmode Sensorer.tex >> $DIR/latexout.txt 2>&1
+pdflatex -interaction=nonstopmode Sensorer.tex >> $DIR/$OUTFILE 2>&1
 cd ../Teknisk_Dokumentation
-pdflatex -interaction=nonstopmode tekdok.tex >> $DIR/latexout.txt 2>&1
+pdflatex -interaction=nonstopmode tekdok.tex >> $DIR/$OUTFILE 2>&1
 cd ../../Designspec
-pdflatex -interaction=nonstopmode Designspec.tex >> $DIR/latexout.txt 2>&1
+pdflatex -interaction=nonstopmode Designspec.tex >> $DIR/$OUTFILE 2>&1
 cd $DIR
-pdflatex -interaction=nonstopmode kappa.tex >> latexout.txt 2>&1 
-grep -w ".*Warning:" latexout.txt
-grep -w ".*Error:" latexout.txt
+pdflatex -interaction=nonstopmode kappa.tex >> $OUTFILE 2>&1 
+grep -w ".*Warning:" $OUTFILE
+grep -w ".*Error:" $OUTFILE
+
+if [ $? != 0 ]
+then
+	rm $OUTFILE
+fi
 
